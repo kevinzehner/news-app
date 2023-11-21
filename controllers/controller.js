b9dacfd99afcd456c975203290c41056b9c5afaf
Kevin Zehner <kevzehner@gmail.com>
2023-11-21 18:53:44 +0000

    completes /api/articles/:article_id/comments endpoint

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,9 @@ const {
   selectTopics,
   selectArticleByID,
   selectArticles,
+  selectComments,
 } = require("../models/model");
+const { checkExists } = require("../utils");
 const endPoints = require("../endpoints.json");
 
 exports.getTopics = (req, res, next) => {
@@ -30,6 +32,16 @@ exports.getArticles = (req, res, next) => {
   selectArticles()
     .then((articles) => {
       res.status(200).send({ articles });
+    })
+    .catch(next);
+};
+
+exports.getComments = (req, res, next) => {
+  const { article_id } = req.params;
+  checkExists("articles", "article_id", article_id)
+    .then(() => selectComments(article_id))
+    .then((comments) => {
+      res.status(200).send({ comments });
     })
     .catch(next);
 };
